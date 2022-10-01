1bef9e9ef71b9b5f01824a2c5971285085d11f66
Christian Palacios <cristhianpalacios3@gmail.com>
2022-10-01 07:59:52 -0500

    Configuracion del entorno.

---
+++ src/main/java/esfot/tesis/botics/BoticsApplication.java
@@ -0,0 +1,36 @@
+package esfot.tesis.botics;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+public class BoticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoticsApplication.class, args);
+	}
+
+	@Autowired
+	Environment environment;
+
+	@Bean
+	public Cloudinary cloudinaryConfig() {
+		Cloudinary cloudinary;
+		Map<String, String> config = new HashMap<>();
+		config.put("cloud_name", environment.getProperty("com.cloudinary.cloud_name"));
+		config.put("api_key", environment.getProperty("com.cloudinary.api_key"));
+		config.put("api_secret", environment.getProperty("com.cloudinary.api_secret"));
+		cloudinary = new Cloudinary(config);
+		return cloudinary;
+	}
+
+}
---
+++ src/test/java/esfot/tesis/botics/BoticsApplicationTests.java
@@ -0,0 +1,13 @@
+package esfot.tesis.botics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
