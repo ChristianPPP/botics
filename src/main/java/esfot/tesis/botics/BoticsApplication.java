9b29582723ebee2c03bb5a277f303c49165b8084
Christian Palacios <cristhianpalacios3@gmail.com>
2022-10-28 23:56:25 -0500

    Swagger y documentacion.

--- src/main/java/esfot/tesis/botics/BoticsApplication.java
+++ src/main/java/esfot/tesis/botics/BoticsApplication.java
@@ -1,6 +1,13 @@
 package esfot.tesis.botics;
 
 import com.cloudinary.Cloudinary;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -48,6 +55,17 @@ public class BoticsApplication {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
 		messageSource.setBasename("/messages");
 		return messageSource;
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().addServersItem(new Server().url("https://botics.loca.lt"))
+				.components(new Components().addSecuritySchemes("Access Token",
+						new SecurityScheme().type(SecurityScheme.Type.APIKEY)
+								.in(SecurityScheme.In.HEADER).bearerFormat("jwt").name("Authorization")))
+				.info(new Info().title("BOTICS API").version("BETA")
+						.license(new License().name("MIT").url("https://esfot.epn.edu.ec")))
+				.addSecurityItem(new SecurityRequirement().addList("Access Token"));
 	}
 
 }
